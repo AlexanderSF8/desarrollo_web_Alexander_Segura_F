0444093bbb41ef72e3d46547d013ca718f9405d6
AlexanderSF8 <alexander.segura@ing.uchile.cl>
2026-05-11 20:08:00 -0400

    feat: integrate database storage for registration and activity file uploads

--- app.py
+++ app.py
@@ -1,15 +1,18 @@
+import flask
 from flask import Flask, render_template, request, redirect, url_for, session, flash
 from werkzeug.utils import secure_filename
 from database.db import SessionLocal
-from database.models import Miembro
+from database.models import Miembro, Actividad, Foto
 import os
 import re
+import json
 
 app = Flask(__name__)
 app.secret_key = 's3cr3t_k3y'  
-UPLOAD_FOLDER = 'static/uploads'
+UPLOAD_FOLDER = os.path.join('static/uploads')
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+os.makedirs('UPLOAD_FOLDER', exist_ok=True)
 
 # Funciones de validación
 def validate_email(email):
@@ -53,39 +56,92 @@ def register():
         if not validate_email(email):
             flash('El correo electrónico no es válido.', 'error')
             return redirect(url_for('register'))
+        
+        db = SessionLocal()
+        # create a new member instance and add it to the database
+        try: 
+            nuevo_miembro = Miembro(
+                nombre=name,
+                email=email,
+                telefono=phone,
+                categoria=category,
+                comuna_id=int(comuna_id)
+            )
+            db.add(nuevo_miembro)
+            db.commit()
 
-        # Guardar la categoría en la sesión
-        session['user_category'] = category
-        flash('Registro exitoso.', 'success')
-        return redirect(url_for('activities'))
+            # Save 
+            session['user_category'] = category
+            session['miembro_id'] = nuevo_miembro.id
+
+            flash('Registro exitoso. Ahora completa este apartado', 'success')
+            return redirect(url_for('activities'))
+        except Exception as e:
+            # error asociate with email uniqueness constraint
+            db.rollback()
+            flash('Error al registrar el miembro, puede que el correo ya esté registrado.', 'error')
+            print(f"Error al registrar miembro: {e}")
+            return redirect(url_for('register'))
+        finally:
+            db.close()
 
     return render_template('register.html')
 
 @app.route('/activities', methods=['GET', 'POST'])
 def activities():
+    # First, verify that the user complete the registration form and has a valid session
+    if 'miembro_id' not in session:
+        flash('Acceso Denegado: debes registrarte primero.', 'error')
+        return redirect(url_for('register'))
+    miembro_id = session.get('miembro_id')
+
+
     if request.method == 'POST':
         activity_type = request.form.getlist('actividades')
         description = request.form.get('activity')
-        files = request.files.getlist('archivos')
+        days_week = request.form.get('dias_semana')
+        hours_days = request.form.get('horas_dia')
+        fotos = request.files.getlist('archivos')
         link = request.form.get('enlace')
+        
+        types_json = json.dumps(activity_type)  #convert list to JSON string for storage
+        db = SessionLocal() 
+        try:
+            nueva_actividad = Actividad(
+                miembro_id=miembro_id,
+                tipo=types_json,
+                descripcion=description,
+                dias_semana=int(days_week),
+                horas_dia=float(hours_days),
+                enlace=link
+            )
+            db.add(nueva_actividad)
+            db.commit()
+            fotos=request.files.getlist('archivos')
+            for foto in fotos:
+                if foto and foto.filename:
+                    filename = secure_filename(foto.filename)
+                    file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+                    foto.save(file_path)
 
-        # Validaciones
-        valid, error = validate_form_data({
-            'Descripción de la Actividad': description,
-            'Enlace': link
-        })
-        if not valid:
-            flash(error, 'error')
+                    nueva_foto = Foto(
+                        ruta_archivo=file_path,
+                        nombre_archivo=filename,
+                        actividad_id=nueva_actividad.id
+                    )
+                    db.add(nueva_foto)
+            db.commit()
+            flash('Actividad registrada exitosamente.', 'success')
+            session.pop('miembro_id', None)  # Clear the session after successful registration
+            return redirect(url_for('index'))
+        
+        except Exception as e:
+            db.rollback()
+            flash('Error al registrar la actividad. Por favor, inténtalo de nuevo.', 'error')
+            print(f"Error al registrar actividad: {e}")
             return redirect(url_for('activities'))
-
-        for file in files:
-            if file.filename:
-                filename = secure_filename(file.filename)
-                file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-
-        flash('Actividad registrada exitosamente.', 'success')
-        return redirect(url_for('list_members'))
-
+        finally:
+            db.close()
     return render_template('activities.html')
 
 @app.route('/list')
